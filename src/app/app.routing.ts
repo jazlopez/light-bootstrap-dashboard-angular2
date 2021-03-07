b7a0ce1ea1934fbc2a8172ede1d42ce3f8b411d6
Jaziel Lopez <juan.jaziel@gmail.com>
2021-03-07 01:26:07 -0800

    login route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,13 +4,15 @@ import { BrowserModule  } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes =[
   {
     path: '',
     redirectTo: 'dashboard',
     pathMatch: 'full',
-  }, {
+  },
+  {
     path: '',
     component: AdminLayoutComponent,
     children: [
@@ -18,6 +20,10 @@ const routes: Routes =[
       path: '',
       loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
   }]},
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: '**',
     redirectTo: 'dashboard'
